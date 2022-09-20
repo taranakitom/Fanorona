05a017172f77c428f8a3e62656f4c60c5890e9b2
taranakitom <tomburgess@slingshot.co.nz>
2022-09-20 20:54:32 +1200

    Highlight selected piece and de select when piece moved

--- fanorona/game.py
+++ fanorona/game.py
@@ -1,6 +1,6 @@
 import pygame
 from .board import Board
-from .constants import BLACK, WHITE, BLUE, COL_RECTS, ROW_RECTS, DOT_SIZE, SPACE_BETWEEN_PIECES, SPACE_LEFT_OF_PIECES, SPACE_ABOVE_PIECES
+from .constants import BLACK, WHITE, BLUE, GREY_RED, BLACK_RED, WHITE_RED, COL_RECTS, ROW_RECTS, DOT_SIZE, SPACE_BETWEEN_PIECES, SPACE_LEFT_OF_PIECES, SPACE_ABOVE_PIECES, PIECE_PADDING, PIECE_OUTLINE
 
 class Game:
     def __init__(self, screen):
@@ -9,7 +9,8 @@ class Game:
     
     def update(self):
         self.board.draw(self.screen)
-        self.draw_valid_moves(self.valid_moves)
+        self.draw_valid_moves()
+        self.draw_selected()
         pygame.display.update()
 
     def _init(self):
@@ -25,12 +26,14 @@ class Game:
         self._init()
     
     def select(self, row, col):
+        # if selecting where to move it
         if self.selected:
             result = self._move(row, col)
-            if not result:
+            if not result: # if not moving to a 0
                 self.selected = None
-                self.select(row, col)
+                self.select(row, col) # select that piece instead
         
+        # if selecting a piece
         piece = self.board.get_piece(row, col)
         if piece != 0 and piece.colour == self.turn:
             self.selected = piece
@@ -46,6 +49,7 @@ class Game:
             self.places_been.append((self.selected.row, self.selected.col))
             self.previous_move = (self.selected.row - row, self.selected.col - col)
             self.board.move(self.selected, row, col)
+            self.selected = None
             taken = self.valid_moves[(row, col)]
             if taken:
                 self.board.remove(taken)
@@ -55,11 +59,19 @@ class Game:
 
         return True
 
-    def draw_valid_moves(self, moves):
-        for move in moves:
+    def draw_valid_moves(self):
+        for move in self.valid_moves:
             row, col = move
             pygame.draw.circle(self.screen, BLUE, (SPACE_LEFT_OF_PIECES + SPACE_BETWEEN_PIECES * col, SPACE_ABOVE_PIECES + SPACE_BETWEEN_PIECES * row), DOT_SIZE)
         
+    def draw_selected(self):
+        if self.selected != None:
+            radius = SPACE_BETWEEN_PIECES // 2 - PIECE_PADDING
+            pygame.draw.circle(self.screen, GREY_RED, (self.selected.x, self.selected.y), radius + PIECE_OUTLINE)
+            if self.turn == WHITE:
+                pygame.draw.circle(self.screen, WHITE_RED, (self.selected.x, self.selected.y), radius)
+            else:
+                pygame.draw.circle(self.screen, BLACK_RED, (self.selected.x, self.selected.y), radius)
 
     def change_turn(self, row, col):
         if len(self.valid_moves[(row, col)]) == 0:
